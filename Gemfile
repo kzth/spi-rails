4d689034e1dc3d7279073ed2f3a9e0512212c9ba
kzt <kzt@kzt-ThinkPad-E420.(none)>
2012-10-25 22:19:29 +0900

    mobile

--- Gemfile
+++ Gemfile
@@ -37,3 +37,6 @@ gem 'jquery-rails'
 
 # To use debugger
 # gem 'debugger'
+
+gem 'mobylette'
+gem 'jquery_mobile_rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 # coding: utf-8
 
 class ApplicationController < ActionController::Base
+  include Mobylette::RespondToMobileRequests
   protect_from_forgery
+
+  mobylette_config do |config|
+    config[:skip_xhr_requests] = false
+  end
 
   # ユーザーのポイントを加算する関数
   def add_point(user_id, p = 10)
